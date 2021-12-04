e98dc390960d48d643fbe48190eb5e89f2472aca
nguyenquyem99dt <nguyenquyem99dt@gmail.com>
2021-12-04 20:45:52 +0700

    add generate images

--- generate.py
+++ generate.py
@@ -402,15 +402,16 @@ if __name__ == "__main__":
                         (z, truncation, truncation_mean_latent),
                         mode="random_generation",
                     )
-
-                    images = images.permute(0, 2, 3, 1)
-                    images = images.cpu()
                     #### new ###
                     for image in images:
                         save_image(image, f'{args.save_image_dir}/random_generation_{seed}_{k}.png')
                     total_images = torch.cat([total_images, images], dim=0)
                     k += 1
                     ############
+                    
+                    images = images.permute(0, 2, 3, 1)
+                    images = images.cpu()
+                    
 
                 total_images = torch.clamp(total_images, min=-1.0, max=1.0)
                 total_images = (total_images + 1) / 2 * 255
